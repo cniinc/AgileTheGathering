31485a7d40fb43b4f9878810c163094d467541dc
Jason Mathias <jason.mathias@gmail.com>
2015-09-06 13:41:30 -0700

    First Commit

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManagerScript : MonoBehaviour {
+
+	public int currentDay;
+	public List<Player> playerList; 
+
+	public enum stateSystem {none, tutorial, decisionStage, workStage};
+	public stateSystem stateMachine;
+	public bool testMode;
+	public bool debugMode;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
