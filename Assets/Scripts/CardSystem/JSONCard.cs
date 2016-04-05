ae65c534dffc5fda2cbc8d03bbd044ee248b452f
Jason M <software@positrongames.com>
2016-04-05 01:17:47 -0700

    started JSON parsing with two scripts: JSONParser and JSONCard. Issue with deserialization - but in theory it should spit out a JSONCard with the data filled in. Figure this hurdle out first

---
+++ Assets/Scripts/CardSystem/JSONCard.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class JSONCard : MonoBehaviour {
+
+	public int id;
+	public string card_type;
+	public string situation;
+	public string choice_1;
+	public int c1_agile_coach_points;
+	public int c1_tech_anchor_points;
+	public int c1_product_owner_points;
+	public int c1_design_lead_points;
+	public int c1_health_impact;
+	public int c1_special_event;
+	public string c1_agile_coach_explanation;
+	public string c1_tech_anchor_explanation;
+	public string c1_product_owner_explanation;
+	public string c1_design_lead_explanation;
+	public string choice_2;
+	public int c2_agile_coach_points;
+	public int c2_tech_anchor_points;
+	public int c2_product_owner_points;
+	public int c2_design_lead_points;
+	public int c2_health_impact;
+	public int c2_special_event;
+	public string c2_agile_coach_explanation;
+	public string c2_tech_anchor_explanation;
+	public string c2_product_owner_explanation;
+	public string c2_design_lead_explanation;
+	public string choice_3;
+	public int c3_agile_coach_points;
+	public int c3_tech_anchor_points;
+	public int c3_product_owner_points;
+	public int c3_design_lead_points;
+	public int c3_health_impact;
+	public int c3_special_event;
+	public string c3_agile_coach_explanation;
+	public string c3_tech_anchor_explanation;
+	public string c3_product_owner_explanation;
+	public string c3_design_lead_explanation;
+	public string url;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void CreateCardObject()
+	{
+		print ("would create card number " + id);
+	}
+
+
+
+}
---
+++ Assets/Scripts/JSONParser.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class JSONParser : MonoBehaviour {
+
+	public GameObject cardObject;
+	public GameObject[] ListOfCards;
+
+	// Use this for initialization
+	void Start () {
+
+		//StartCoroutine (PullWholeCardList ());
+
+		for (int id = 44; id < 50; id++) {
+			print ("ID: " + id);
+			StartCoroutine (CreateCardsFromURLs (id));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator PullWholeCardList()
+	{
+		WWW cardList = new WWW ("http://gameschool.herokuapp.com/cards.json");
+		yield return cardList;
+		CreateWholeFromJSON (cardList.text);
+	}
+
+
+
+	public void CreateWholeFromJSON(string cardList)
+	{
+		JSONCard jCard = JsonUtility.FromJson<JSONCard> (cardList);
+		GameObject card = Instantiate (cardObject, Vector3.zero, Quaternion.identity) as GameObject;
+	}
+
+
+
+	public IEnumerator CreateCardsFromURLs(int id) 
+	{
+		
+		print ("pulling data of card " + id);
+		WWW cardURL = new WWW ("http://gameschool.herokuapp.com/cards/" + id + ".json");
+		yield return cardURL;
+		JSONCard jCard = JsonUtility.FromJson<JSONCard> (cardURL.text);
+		jCard.CreateCardObject ();
+
+		
+	}
+}
